2770dc82098d7631a81ac5e3a7fe7e3817ed5065
zhuo.zhang3 <zhuo.zhang3@renren-inc.com>
2018-01-13 20:12:29 +0800

    更改header 组建

--- sianfront/config/index.js
+++ sianfront/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/liseloveH5/sianweb',
+    assetsPublicPath: '/sianweb',
 
     /**
      * Source Maps
