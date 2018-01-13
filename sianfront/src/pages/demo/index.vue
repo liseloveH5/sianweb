4a210d27c21687d03f1b224fb1527c7be479bed3
zhuo.zhang3 <zhuo.zhang3@renren-inc.com>
2018-01-13 15:27:59 +0800

    增加基本样式

--- sianfront/src/pages/demo/index.vue
+++ sianfront/src/pages/demo/index.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="demo">
-    <b-btn v-b-modal.modal1>Launch demo modal</b-btn>
-
-    <!-- Modal Component -->
-    <b-modal id="modal1" title="Bootstrap-Vue">
-      <p class="my-4">Hello from modal!</p>
-    </b-modal>
+    <h1>111</h1>
 
   </div>
 </template>
@@ -16,5 +11,5 @@
 </script>
 
 <style lang="less">
-  @import "index.less";
+  @import "./index.less";
 </style>
---
+++ sianfront/src/pages/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="demo">
+    <b-btn v-b-modal.modal1>Launch demo modal</b-btn>
+
+    <!-- Modal Component -->
+    <b-modal id="modal1" title="Bootstrap-Vue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+
+  </div>
+</template>
+
+<script>
+  import index from './index.js'
+  export default index
+</script>
+
+<style lang="less">
+  @import "index.less";
+</style>
