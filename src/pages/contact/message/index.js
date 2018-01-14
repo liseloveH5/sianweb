db3adb2455e460a46c4366d8f884eb7305946832
zhuo.zhang3 <zhuo.zhang3@renren-inc.com>
2018-01-14 15:09:28 +0800

    全局样式更改

--- src/pages/contact/message/index.js
+++ src/pages/contact/message/index.js
@@ -6,15 +6,11 @@ const index = {
       form: {
         email: '',
         name: '',
-        food: null,
-        checked: []
+        message:'',
+        phone:'',
+        tel:'',
       },
-      foods: [
-        { text: 'Select One', value: null },
-        'Carrots', 'Beans', 'Tomatoes', 'Corn'
-      ],
       show: true,
-      text: '',
     }
   },
 
@@ -34,18 +30,8 @@ const index = {
     onSubmit (evt) {
       evt.preventDefault();
       alert(JSON.stringify(this.form));
-    },
-    onReset (evt) {
-      evt.preventDefault();
-      /* Reset our form values */
-      this.form.email = '';
-      this.form.name = '';
-      this.form.food = null;
-      this.form.checked = [];
-      /* Trick to reset/clear native browser form validation state */
-      this.show = false;
-      this.$nextTick(() => { this.show = true });
     }
+
   }
 
 };
--- src/pages/contact/message/index.vue
+++ src/pages/contact/message/index.vue
@@ -3,53 +3,103 @@
     <global-header ></global-header>
     <div class="common-contain">
        <div class="container">
-         <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="message-form">
+         <b-form @submit="onSubmit"  v-if="show" class="message-form">
+
+           <b-form-group id="exampleInputGroup1"
+                         label="Email address:"
+                         label-for="exampleInput1"
+                         description="We'll never share your email with anyone else.">
+             <b-form-input id="exampleInput1"
+                           type="email"
+                           v-model="form.email"
+                           required
+                           placeholder="Enter email">
+             </b-form-input>
+           </b-form-group>
 
 
            <b-form-row>
              <b-col sm="1">
-               <label :for="textarea1">Type :</label>
+             <label :for="form-email">邮箱</label>
              </b-col>
-             <b-col sm="9">
-               <b-form-textarea id="textarea1"
-                                v-model="text"
-                                placeholder="Enter something"
-                                :rows="3"
-                                :max-rows="6">
-               </b-form-textarea>
-             </b-col>
+           <b-col sm="4">
+           <b-form-input id="form-email"
+           type="text"
+           v-model="form.email"
+           placeholder="请输入邮箱">
+           </b-form-input>
+           </b-col>
            </b-form-row>
 
+           <!--<b-form-row>-->
+           <!--<b-col sm="1">-->
+             <!--<label :for="form-message">留言内容</label>-->
+           <!--</b-col>-->
+           <!--<b-col sm="11">-->
+             <!--<b-form-textarea id="form-message"-->
+                              <!--v-model="form.message"-->
+                              <!--placeholder="Enter something"-->
+                              <!--:rows="3"-->
+                              <!--:max-rows="6">-->
+             <!--</b-form-textarea>-->
+           <!--</b-col>-->
+         <!--</b-form-row>-->
 
+           <!--<span>{{form.message}}</span>-->
 
+           <!--<b-form-row>-->
+             <!--<b-col sm="1">-->
+               <!--<label :for="form-name">姓名</label>-->
+             <!--</b-col>-->
+             <!--<b-col sm="4">-->
+               <!--<b-form-input id="form-name"-->
+                             <!--type="text"-->
+                             <!--v-model="form.name"-->
+                             <!--placeholder="请输入姓名">-->
+               <!--</b-form-input>-->
+             <!--</b-col>-->
+           <!--</b-form-row>-->
 
-           <b-form-group id="exampleInputGroup2"
-                         label="Your Name:"
-                         label-for="exampleInput2">
-             <b-form-input id="exampleInput2"
-                           type="text"
-                           v-model="form.name"
-                           required
-                           placeholder="Enter name">
-             </b-form-input>
-           </b-form-group>
-           <b-form-group id="exampleInputGroup3"
-                         label="Food:"
-                         label-for="exampleInput3">
-             <b-form-select id="exampleInput3"
-                            :options="foods"
-                            required
-                            v-model="form.food">
-             </b-form-select>
-           </b-form-group>
-           <b-form-group id="exampleGroup4">
-             <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
-               <b-form-checkbox value="me">Check me out</b-form-checkbox>
-               <b-form-checkbox value="that">Check that out</b-form-checkbox>
-             </b-form-checkbox-group>
-           </b-form-group>
-           <b-button type="submit" variant="primary">Submit</b-button>
-           <b-button type="reset" variant="danger">Reset</b-button>
+           <!--<b-form-row>-->
+             <!--<b-col sm="1">-->
+               <!--<label :for="form-phone">手机</label>-->
+             <!--</b-col>-->
+             <!--<b-col sm="4">-->
+               <!--<b-form-input id="form-phone"-->
+                             <!--type="text"-->
+                             <!--v-model="form.phone"-->
+                             <!--placeholder="请输入手机号码">-->
+               <!--</b-form-input>-->
+             <!--</b-col>-->
+           <!--</b-form-row>-->
+
+           <!--<b-form-row>-->
+             <!--<b-col sm="1">-->
+               <!--<label :for="form-tel">电话</label>-->
+             <!--</b-col>-->
+             <!--<b-col sm="4">-->
+               <!--<b-form-input id="form-tel"-->
+                             <!--type="text"-->
+                             <!--v-model="form.tel"-->
+                             <!--placeholder="请输入电话号码">-->
+               <!--</b-form-input>-->
+             <!--</b-col>-->
+           <!--</b-form-row>-->
+
+           <!--<b-form-row>-->
+             <!--<b-col sm="1">-->
+               <!--<label :for="form-email">邮箱</label>-->
+             <!--</b-col>-->
+             <!--<b-col sm="4">-->
+               <!--<b-form-input id="form-email"-->
+                             <!--type="text"-->
+                             <!--v-model="form.email"-->
+                             <!--placeholder="请输入邮箱">-->
+               <!--</b-form-input>-->
+             <!--</b-col>-->
+           <!--</b-form-row>-->
+
+           <b-button type="submit" variant="primary">提交</b-button>
          </b-form>
        </div>
     </div>
